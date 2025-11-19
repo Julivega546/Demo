c36ce38841abaa7dbd224721a892be92eeeb8312
Melipilla <melipilla@duoc.acad>
2025-11-19 17:20:38 -0300

    Initial commit

---
+++ demo/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+
+
+
+}
